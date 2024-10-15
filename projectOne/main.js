b36f2f6484d9c084770dc13e1b81cc8aa6c3c6a0
WolfgangLoud <wolo8873@colorado.edu>
2024-10-15 16:59:45 -0600

    updaye project

--- projectOne/main.js
+++ projectOne/main.js
@@ -1,11 +1,14 @@
 import anime from 'animejs/lib/anime.es.js';
 
 const animation = anime({targets:".img"
-, translateY: 800, duration: 4000}
+, translateY: 800, duration: 4000, autoplay: false}
 )
 
+const section2= document.querySelector(".section-two");
+
 window.onscroll = function() {
-  console.log(window.scrollY)
-  // animation.seek((scrollPercent / 100) * animation.duration);
+  const scrollPercent = window.scrollY-section2.offsetTop
+ 
+  animation.seek((scrollPercent / 100) * animation.duration);
 }
 
