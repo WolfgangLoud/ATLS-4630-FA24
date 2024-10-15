c2d49293b052d7e3356830439f7d3628e034a734
WolfgangLoud <wolo8873@colorado.edu>
2024-10-15 17:21:47 -0600

    Update main.js

--- projectOne/main.js
+++ projectOne/main.js
@@ -1,14 +1,37 @@
 import anime from 'animejs/lib/anime.es.js';
 
+
+//Will probably use this code to improve functionality of offsetTop
+// const getOffsetTop = element => {
+//   let offsetTop = 0;
+//   while(element) {
+//     offsetTop += element.offsetTop;
+//     element = element.offsetParent;
+//   }
+//   return offsetTop;
+// }
+
+// const X = getOffsetTop(someElement);
+
+const section2= document.querySelector("#section-two");
+
+
 const animation = anime({targets:".img"
-, translateY: 800, duration: 4000, autoplay: false}
+, translateX: 3000, duration: 4000, autoplay: false}
 )
 
-const section2= document.querySelector(".section-two");
+const speed1 = 800;
+const offset1 = 300;
+
+
+
+
+const animateScroll = function(div, speed=100, offset=0){
+  const scrollPercent = window.scrollY-div.offsetTop
+  return((scrollPercent+offset)/speed);
+}
 
 window.onscroll = function() {
-  const scrollPercent = window.scrollY-section2.offsetTop
- 
-  animation.seek((scrollPercent / 100) * animation.duration);
-}
+  animation.seek(animateScroll(section2,speed1,offset1) * animation.duration);
+} 
 
