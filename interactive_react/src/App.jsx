d8a8ce00f9ba08dfb546f7a9dbf2e964dccefc3f
WolfgangLoud <wolo8873@colorado.edu>
2024-11-21 13:45:00 -0700

    fixed

--- interactive_react/src/App.jsx
+++ interactive_react/src/App.jsx
@@ -2,24 +2,64 @@ import "./App.css";
 import Dropdown from "./Dropdown";
 import DropdownItem from "./DropdownItem";
 
+const data =[
+  {
+    category: 'mammal',
+    name: 'Cat',
+  },
+  {
+    category: 'mammal',
+    name:'Dog'
+  },
+  {
+    category: 'mammal',
+    name:'Monkey'
+  },
+  {
+    category: 'reptile',
+    name: 'Snake',
+  },
+  {
+    category: 'reptile',
+    name: 'Lizzard',
+  },
+  {
+    category: 'reptile',
+    name: 'Turtle',
+  }
+]
+
+
 export default function App() {
   
-  const items = ["cat", "dog", "meow", "woof", "bark"];
+  const mammals = data.filter(item => item.category === 'mammal');
+  const reptiles = data.filter(item => item.category === 'reptile');
 
   return (
     <div className="App">
+      <h1 className="title">My Awesome Drop Downs</h1>
       <div className="content">
         <Dropdown 
-          buttonText="Dropdown button" 
+          buttonText="Mammals" 
           content={
             <>
-              {items.map((item) => (
-                <DropdownItem key={item} item={item}/>
+              {mammals.map((mammal) => (
+                <DropdownItem key={mammal.name} item={mammal.name} />
+              ))}
+            </>
+          }
+        />
+        <Dropdown 
+          buttonText="Reptiles" 
+          content={
+            <>
+              {reptiles.map((reptile) => (
+                <DropdownItem key={reptile.name} item={reptile.name} />
               ))}
             </>
           }
         />
       </div>
     </div>
-  );
+  )
 }
